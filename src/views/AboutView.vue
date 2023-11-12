9af061b0228e2ebaea3548263a053035f04a73b6
kun99 <kuns.km@gmail.com>
2023-11-12 08:03:16 +0700

    About page update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="flex justify-center">
-    <h1>This is an about page</h1>
+  <div class="flex flex-col items-center justify-center pt-10">
+    <h1 class="text-3xl">ICCS340 Pokédex</h1>
+    <h2 class="text-xl pt-4">M11-Assignment - Geon</h2>
+    <div class="flex flex-col items-center">
+      <div class="flex flex-row pt-4">
+        <a
+          href="https://github.com/kun99/pokemon-data"
+          class="bg-zinc-200 border rounded-lg underline px-2 mx-2"
+          target="_blank"
+        >
+          db.json
+        </a>
+        <a
+          href="https://github.com/kun99/pokedex"
+          class="bg-zinc-200 border rounded-lg underline px-2 mx-2"
+          target="_blank"
+        >
+          Github repo
+        </a>
+        <a
+          href=""
+          class="bg-zinc-200 border rounded-lg underline px-2 mx-2"
+          target="_blank"
+        >
+          Site URL
+        </a>
+      </div>
+    </div>
   </div>
 </template>
